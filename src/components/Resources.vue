bf545454a798813cc26e5703bf9b50bed8ff3222
Nigel Stirzaker <nrstirzaker.techblog@gmail.com>
2020-04-24 21:29:39 +0100

    drop down fixed and waiting message added

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,48 +1,64 @@
 <template>
-  <ul id="resource">
-    <div
-      v-for="resource in resources"
-      :key="resource.RowId"
-    >
-      <div class="q-pa-md row items-start q-gutter-md">
-        <q-card
-          class="my-card"
-          flat
-          bordered
-        >
-          <q-card-section class="q-pt-xs">
-            <q-card-section vertical>
-              <div class="text-overline">Topic: {{resource.Topic}}</div>
-              <div class="text-h5 q-mt-sm q-mb-xs">Name: {{ resource.Name }}</div>
-              <div class="text-h6 q-mt-sm q-mb-xs">Address: <a :href='resource.Address'>{{ resource.Address }}</a></div>
-              <div class="text-caption text-grey">
-                {{resource.Notes}}
-              </div>
-            </q-card-section>
-            <q-card-actions
-              horizontal
-              class="justify-around"
-            >
-              <q-btn
-                flat
-                round
-                color="red"
-                icon="favorite"
-              />
-              <q-btn
-                flat
-                round
-                color="primary"
-                icon="share"
-              />
-            </q-card-actions>
-          </q-card-section>
-        </q-card>
-      </div>
+  <div v-if="isLoaded">
+    <ul id="resource">
 
+      <div
+        v-for="resource in resources"
+        :key="resource.RowId"
+      >
+        <div class="q-pa-md row items-start q-gutter-md">
+          <q-card
+            class="my-card"
+            flat
+            bordered
+          >
+            <q-card-section class="q-pt-xs">
+              <q-card-section vertical>
+                <div class="text-overline">Topic: {{resource.Topic}}</div>
+                <div class="text-h5 q-mt-sm q-mb-xs">Name: {{ resource.Name }}</div>
+                <div class="text-h6 q-mt-sm q-mb-xs">Address: <a :href='resource.Address'>{{ resource.Address }}</a></div>
+                <div class="text-caption text-grey">
+                  {{resource.Notes}}
+                </div>
+              </q-card-section>
+              <q-card-actions
+                horizontal
+                class="justify-around"
+              >
+                <q-btn
+                  flat
+                  round
+                  color="red"
+                  icon="favorite"
+                />
+                <q-btn
+                  flat
+                  round
+                  color="primary"
+                  icon="share"
+                />
+              </q-card-actions>
+            </q-card-section>
+          </q-card>
+        </div>
+
+      </div>
+    </ul>
+  </div>
+
+  <div v-else>
+    <div class="waiting">
+      Please wait while data is Loading...
     </div>
-  </ul>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.waiting {
+  color: $primary;
+  font-size: 21px;
+}
+</style>
 
 <script>
 import axios from 'axios'
@@ -51,6 +67,7 @@ export default {
   name: 'Resources',
   data () {
     return {
+      isLoaded: false,
       resources: null
     }
   },
@@ -66,6 +83,7 @@ export default {
         .get(restURL)
         .then(response => {
           //  console.log(JSON.stringify(response.data.rows))
+          this.isLoaded = true
           this.resources = response.data.rows
         })
     }
@@ -78,7 +96,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
